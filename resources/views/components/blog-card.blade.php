efa3f20a32223f69290e8f39591656111838824e
rrivera <rrivera@redbannermedia.com>
2024-03-10 07:45:39 -0400

    Working on creating blog cards

--- resources/views/components/blog-card.blade.php
+++ resources/views/components/blog-card.blade.php
@@ -1,16 +1,29 @@
 @props([
     'title',
     'content',
-    'direction'
+    'direction',
+    'blogLink',
+    'blogImg'
 ])
 {{--TODO: finish direction flow--}}
-<article {{ $attributes->class(['mt-16 pb-12 pt-16 border-l-8 border-l-red-700 grid justify-center space-y-4
-                   w-8/12 mx-auto bg-gradient-to-r to-[#22222E] from-[#3F3D56] relative shadow-lg']) }}
+<article
+    {{ $attributes->class([
+       'flex flex-col w-3/12 grow'=> $direction === 'col',
+       'flex flex-row w-8/12 mx-auto'=> $direction === 'row',
+        'my-12 border-l-8 border-l-red-700 bg-gradient-to-r to-[#22222E] from-[#3F3D56] shadow-lg space-x-8'
+        ])
+    }}
 >
-    <img src="{{@asset('./images/blogImg.jpg')}}" alt="Blog Img">
-    <div>
-        <h3 class="text-2xl text-yellow-500">{{$title}}</h3>
-        <p class="text-white">{{$content}}</p>
+    <img
+        class="h-auto w-72"
+        src="{{$blogImg}}"
+        alt="Blog Img"
+    >
+    <div class="self-center flex flex-col grow px-16 space-y-6">
+        <div class="grow">
+            <h3 class="text-2xl text-yellow-500">{{$title}}</h3>
+            <p class="text-white">{{$content}}</p>
+        </div>
+        <a href="{{$blogLink}}" class="w-full text-red-600 font-bold text-xl text-right">READ MORE</a>
     </div>
-    <a href="#" class="w-full text-right">READ MORE</a>
 </article>
--- resources/views/livewire/blog.blade.php
+++ resources/views/livewire/blog.blade.php
@@ -1,4 +1,19 @@
+@php
+    //TODO: be sure to remove this @php block as it is only for testing in dev
+    $title = 'Blog Title 1';
+    $content = 'Lorem Ipsum dolores asdfa adf asdfas d';
+    $blogLink = '#';
+    $blogImg = @asset('./images/blogImg.jpg');
+@endphp
+
 <section>
-    {{--TODO: Examples of content cards--}}
-    <x-blog-card :title="$title" :content="$content" direction="horizontal"/>
+    {{--TODO: Continue to work on direction="col" direction="row" be sure to test thoroughly--}}
+    <div class="h-3/4">
+        <x-blog-card
+            :title="$title"
+            :content="$content"
+            :blog-link="$blogLink"
+            direction="col"
+            :blog-img="@asset('images/blogImg.jpg')"/>
+    </div>
 </section>
