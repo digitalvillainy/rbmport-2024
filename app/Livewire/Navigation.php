33e9a67dd85ba8ae62a85132477accfe629b0f34
Roberto Rivera <rrivera@redbannermedia.com>
2024-03-15 02:58:49 -0400

    Updated navigation to work properly with #

--- app/Livewire/Navigation.php
+++ app/Livewire/Navigation.php
@@ -7,6 +7,15 @@ use Livewire\Component;
 
 class Navigation extends Component
 {
+    public bool $active = false;
+
+    public bool $current;
+
+    // Content created in alpine
+    public function userSelected(): void
+    {
+    }
+
     public function render(): View
     {
         return view('livewire.navigation');
--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,11 +1,51 @@
 <nav class="px-5 bg-[#525470] bg-opacity-65 relative h-20 shadow-2xl">
     <a href="/">
-        <img class="absolute -top-1 h-28 w-40 z-10" src="{{asset('./icons/rbm-logo.svg')}}" alt="Red Banner Media, LLC Logo">
+        <img class="absolute -top-1 h-28 w-40 z-10" src="{{asset('./icons/rbm-logo.svg')}}"
+             alt="Red Banner Media, LLC Logo">
     </a>
-    <ul class="absolute right-0 flex flex-row space-x-5 align-middle min-h-full mr-5">
-        <livewire:nav-btn route-href="/" title="build-a-site"/>
-        <livewire:nav-btn route-href="/#hired" title="hire-a-dev"/>
-        <livewire:nav-btn route-href="/#faq" title="contact"/>
-        <livewire:nav-btn route-href="/blog" title="blog"/>
+    <ul x-data="{
+            current: $wire.entangle('current'),
+            init(){
+                this.current = window.location.hash.length !== 0 ? window.location.hash : window.location.pathname;
+            },
+            updateByUser(target){
+               this.current = target;
+            }
+        }"
+        class="absolute right-0 flex flex-row space-x-5 align-middle min-h-full mr-5">
+        <li class="flex flex-col self-center">
+            <a
+                :class="current === '/' ? 'build-a-site-active' : 'nav-btn' "
+                href="/"
+            >
+                BUILD A SITE
+            </a>
+        </li>
+        <li class="flex flex-col self-center">
+            <a
+                :class="current === '#hired' ? 'hire-a-dev-active' : 'nav-btn' "
+                @click="current = '#hired'"
+                href="/#hired"
+            >
+                HIRE A DEV
+            </a>
+        </li>
+        <li class="flex flex-col self-center">
+            <a
+                :class="current === '#faq' ? 'contact-active' : 'nav-btn' "
+                @click="current = '#faq'"
+                href="/#faq"
+            >
+                CONTACT
+            </a>
+        </li>
+        <li class="flex flex-col self-center">
+            <a
+                :class="current === '/blog' ? 'blog-active' : 'nav-btn' "
+                href="/blog"
+            >
+                BLOG
+            </a>
+        </li>
     </ul>
 </nav>
